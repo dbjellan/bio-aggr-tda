b74cd43210900b27044041c895a1b0fe75a96d82
Devin Bjelland <dbjellan@macalester.edu>
2015-06-03 10:53:33 -0500

    Added commenting

--- make_crocker.m
+++ make_crocker.m
@@ -1,38 +1,43 @@
 
-timesamples = 1000;
-filtrationsamples = 500;
 
+% load data from our experiment
 expnum = 1;
 aphiddata = load('aphiddata.csv');
 indx = (aphiddata(:, 1) == expnum);
 ourdata = aphiddata(indx, [3 4 5]);
-
 endframe = max(ourdata(:, 1));
 
+% set up dimensions of matrix for crocker plot
 maxfiltration = .3;
-filtrationgap = .01;
+filtrationgap = .001;
 filtrationtimes = 0:filtrationgap:maxfiltration;
-
+timesamples = 2000;
 crocker_dat = 0*ones(length(filtrationtimes), timesamples);
 
 for i = 1:timesamples,
+    % convert sample number to frame number and extract the x, y data to <thisframe>
     framenum = sampletoframe(i, timesamples, endframe);
     indx = (ourdata(:, 1) == framenum);
     thisframe = ourdata(indx, [2 3]);
 
+    % calculate persistent homology on this frame
     m_space = metric.impl.EuclideanMetricSpace(thisframe);
-    stream = api.Plex4.createVietorisRipsStream(thisframe, 2, 4, 500);
+    stream = api.Plex4.createVietorisRipsStream(thisframe, 2, 4, 1000);
     persistence = api.Plex4.getModularSimplicialAlgorithm(3, 2);
     intervals = persistence.computeIntervals(stream);
     
+    % get barcode's endpoints, and calculate how many intersect with each
+    % filtration time that we sample
     endpoints = edu.stanford.math.plex4.homology.barcodes.BarcodeUtility.getEndpoints(intervals, 0, 0);
     for j = 1:size(endpoints, 1),
-        thisendpoint = endpoints(j, :);
+        % increments all filtrations times, for a fixed persistence
+        % interval and frame, that intersect the interval
         indx = (filtrationtimes >= endpoints(j, 1) & (filtrationtimes <= endpoints(j, 2)));
         crocker_dat(:, i) = crocker_dat(:, i) + indx';
     end
     
 end
 
+% make our contour plot with provided contours 
 contour(crocker_dat, [1 2 3 4 5]);
     
--- sampletoframe.m
+++ sampletoframe.m
@@ -1,3 +1,5 @@
+% converts sample number to frame number. Minimum frame is 2 since that is
+% the first collected frame in our dataset
 function r = sampletoframe(sample, numsamples, numframes)
-    r = floor(sample * numframes / numsamples);
+    r = max(2, floor(sample * numframes / numsamples));
 end
