7f00de23bfe772d377888df31a39650f8831aaed
marcvsavrelivs <mulmer@macalester.edu>
2015-06-22 16:00:23 -0500

    Update dOrsogna.m
    Now works. Primarily authored by Chad.

--- dOrsogna.m
+++ dOrsogna.m
@@ -1,89 +1,65 @@
 function dOrsogna()
-numparticles = 100;
-%define tstart, tend and the number of time steps you want to take, n:
-tstart = 1;
-tend = 20;
-n = 500;
+N = 100;
+tstart = 0;
+tend = 34;
 m = 1;
-tspan = linspace(tstart,tend,n);
 
 %define the initial conditions making sure to use the right ordering
-xinit = 10*rand(numparticles, 1);
-yinit = 10*rand(numparticles, 1);
-%SHOULDN'T THESE TWO BE GENERATED FROM THE ABOVE?
-vxinit = 2*pi*rand(numparticles, 1);
-vyinit = 2*pi*rand(numparticles, 1);
+xinit = 10*rand(N, 1);
+yinit = 10*rand(N, 1);
+
+vxinit = 2*rand(N, 1) - 1;
+vyinit = 2*rand(N, 1) - 1;
+
+
 
 in = [vxinit; vyinit; xinit; yinit];
-in = in';
 
-[t, x] = ode45(@functionOfInterest, tspan, in);
-matrix = [t, x];
+[t, x] = ode45(@functionOfInterest, [tstart,tend], in, [], N);
 
-y = zeros(1, 4*numparticles);
-
-size([t, x])
-for theta = 1:n
-    for i = 1:numparticles
-        
-        y = matrix(theta, :);
-        
-        k = numparticles;
-        newvx = y(1:k);
-        newvy = y(k+1:2*k);
-        newxpos = y(2*k+1:3*k);
-        newypos = y(3*k+1:4*k);
-        
-        %there has GOT to be a more efficient way to do this
-        points(i, :) = [newxpos(i), newypos(i)];
-    end
+for theta = 1:length(t)
+    xpos = x(theta,2*N+1:3*N);
+    ypos = x(theta,3*N+1:4*N);
+%     xpos = xpos - mean(xpos);
+%     ypos = ypos - mean(ypos);
+    plot(xpos,ypos,'ko');
     
-    scatter(points(:,1), points(:,2))
-    axis([-100 100 -100 100]);
-    pause(1);
-    
+    %axis([-2 12 -2 12]);
+    %axis([0 8 0 8]);
+    %axis([-1 1 -1 1]);
+    axis([4 6 4 6]);
+    pause(0.01);
+end
+
 end
 
 
-function dxdt = functionOfInterest(t, x)
-    selfPropulsion = 1.5;
-    friction = 1;
-    repulsion = .3;
-    repulsionLength = .5;
-    attraction = .5;
-    attractionLength = 2;
+function dxdt = functionOfInterest(t, x, N)
+selfPropulsion = 1;
+friction = 0.5;
+repulsion = .5;
+repulsionLength = .5;
+attraction = 1;
+attractionLength = 1;
 
-    Q = zeros(numparticles, 2);
-    
-    %size(Q(6,:))
+vx = x(1:N);
+vy = x(N+1:2*N);
+xpos = x(2*N+1:3*N);
+ypos = x(3*N+1:4*N);
 
-    k = numparticles;
-    vx = x(1:k);
-    vy = x(k+1:2*k);
-    xpos = x(2*k+1:3*k);
-    ypos = x(3*k+1:4*k);
-    pos = [xpos, ypos];
+z = xpos + i*ypos;
+v = vx + i*vy;
 
-    xdot = [];
-    vdot = [];
-    vdotxy = [0;0];
+zdot = v;
+[Z1,Z2] = meshgrid(z,z);
+Rdiff = Z2-Z1;
+R = abs(Rdiff);
 
-    for i = 1:numparticles 
+MOI = -repulsion/repulsionLength*exp(-R/repulsionLength)+attraction/attractionLength*exp(-R/attractionLength);
+unitVectors = Rdiff./R;
+unitVectors(logical(eye(size(unitVectors)))) = 0;
+vdot = (selfPropulsion - friction*abs(v).^2).*v - sum(MOI.*unitVectors,2); % the minus sign comes form the minus sign in front of the gradient term in the master equation
 
-        %establish Q forces for each i
-        for j = 1:numparticles
-            if (j ~= i)
-                %Q(i) = Q(i) + repulsion * exp(-norm(pos(i,:) - pos(j,:))/repulsionLength) - attraction * exp(- norm(pos(i,:) - pos(j,:))/attractionLength);
-                %size((repulsion * exp(-norm(pos(i,:) - pos(j,:))/repulsionLength) /repulsionLength - attraction * exp(-norm(pos(i,:) - pos(j,:))/attractionLength) / attractionLength)*((pos(i,:) - pos(j,:))/(norm(pos(i,:) - pos(j,:)))))
-                Q(i,:) = Q(i,:) + (-repulsion * exp(-norm(pos(i,:) - pos(j,:))/repulsionLength) /repulsionLength + attraction * exp(-norm(pos(i,:) - pos(j,:))/attractionLength) / attractionLength)*((pos(i,:) - pos(j,:))/(norm(pos(i,:) - pos(j,:))));
-            end
-        end
+dxdt = [real(vdot);imag(vdot);real(zdot);imag(zdot)];
 
-        xdot = [xdot, vx(i), vy(i)];
-        vdotxy = ([xdot(2*i - 1), xdot(2*i)]/m)*(selfPropulsion - friction*norm([xdot(2*i - 1), xdot(2*i)])^2) - Q(i)/m;
-        vdot = [vdot, vdotxy(1), vdotxy(2)];
-
-    end
-    dxdt = [xdot, vdot]';
-end
 end
